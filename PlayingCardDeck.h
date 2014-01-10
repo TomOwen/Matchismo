9c7e0c7a968b96c7c97853f687d213c4c0fce0f8
Tom Owen <tho@cox.net>
2014-01-10 10:38:35 -0800

    final version

---
+++ PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Tom Owen on 1/10/14.
+//  Copyright (c) 2014 Tom Owen. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
